03ef06c824fb575f48ad6761d913d19a4d64fa28
Akriti Sehrawat <sehrawat@usc.edu>
2013-10-30 11:06:21 -0700

    Added rule for Buzz

--- fizzbuzz.java
+++ fizzbuzz.java
@@ -1,7 +1,9 @@
-public class Fizzbuzz {
-    public static void main(String[] args) {
-	for (int i = 0; i < 100; i++) {
-	    System.out.println(i);
+
+public class FizzBuzz {
+	public static void main(String[] args) {
+		for(int i = 0; i < 100; i++) {
+		if( i % 5 == 0)
+			System.out.println("Buzz");
+		}
 	}
-    }
 }
